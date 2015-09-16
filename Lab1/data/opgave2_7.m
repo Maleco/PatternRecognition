ceb2526ffbf20d22654924b16b4b6ed39efdcf65
Maikel School <m.l.c.withagen@student.rug.nl>
2015-09-16 17:33:55 +0200

    Matlab code ass 2_7

--- Lab1/data/opgave2_7.m
+++ Lab1/data/opgave2_7.m
@@ -1,19 +1,12 @@
-hold off;
+% Calculate the cumulative distribution function for the given x-value
+% (range 0.000 - 1.000, with stepsize 0.001)
+normc = normcdf([0:0.001:1],mean(hd_d),std(hd_d));
+% Count the amount of x-values below the threshold value, minus 1 to
+% correct from starting from 0.000, and divide by 1000 to get a nice 
+% chance number
+d = (sum(normc(:) <= 0.0005)-1)/1000;d
 
-x1 = sum(hd_d(:) == mode(hd_d));
-normd = normpdf([0:0.01:1],mean(hd_d),std(hd_d));
-normd=normd*(x1/max(normd));
-
-y = normcdf(hd_d, mean(hd_d), std(hd_d));
-plot(y);
-
-covd = cov(hd_d);
-
-%[p,plo,pup] = normcdf(hd_d,mean(hd_d),std(hd_d),cov(normd),0.00025);
-
-%plot(erf(hd_d));
-
-erfd = sqrt(2)*erfinv(hd_d);
-plot(erfd.');
-
-ci = paramci(hd_d, ,99.95);
+% Same as before, now for set S
+normc = normcdf([0:0.001:1],mean(hd_s),std(hd_s));
+% False rejection is integral of HD > d = 1 - normc(d)
+1 - normc((d*1000)+1)
