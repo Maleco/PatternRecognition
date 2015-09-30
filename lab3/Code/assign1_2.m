1907f10dbea0a2a190ea2f0da9202b056d67635f
Maikel School <m.l.c.withagen@student.rug.nl>
2015-09-30 12:08:41 +0200

    update

--- lab3/Code/assign1_2.m
+++ lab3/Code/assign1_2.m
@@ -21,6 +21,6 @@ for xStar = -100:.1:100
     y4(end+1) = (1 - normcdf(xStar, 8, 2));
 end 
 plot(x,y4)
-
+legend('random', '\mu2 = 7','\mu2 = 9','\mu2 = 11', 'calculated (fa, h) point', 'ROC curve of estimated d accent', 'Location','east')
 % So d' is 1.5
 % Found by trial and error with two distributions with sigma = 1.
