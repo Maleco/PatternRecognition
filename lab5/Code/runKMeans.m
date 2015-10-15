a323e26d53de684bcd5de8675fa03d5afad96cc0
Maikel School <m.l.c.withagen@student.rug.nl>
2015-10-15 11:42:58 +0200

    local changes

--- lab5/Code/runKMeans.m
+++ lab5/Code/runKMeans.m
@@ -7,7 +7,7 @@ R = zeros(1,kmax);
 
 for k = 1 : kmax
     for i = 1:10
-       error(i) = kmeans(kmeans1,k, 0);
+       error(i) = kmeans(kmeans1, 100, 1, 0);
     end
     J(k) = mean(error);
     R(k) = J(k) * k^(-2/ndims(kmeans1));
@@ -17,3 +17,25 @@ D = R./J;
 
 figure(3)
 plot(D);
+
+% Perform the kmeans++ test
+k = 100;
+nRuns = 20;
+error_without = zeros(1,nRuns);
+error_with    = zeros(1,nRuns);
+for i = 1:nRuns
+    datestr(now)
+    i
+    error_without(i) = kmeans(checkerboard, k, 0, 0);
+    error_with(i)    = kmeans(checkerboard, k, 1, 0);
+end
+error_without = error_without/nRuns;
+error_with = error_with/nRuns;
+
+mean(error_without)
+std(error_without)
+mean(error_with)
+std(error_with)
+
+% using an unpaired one-tailed two-sample t-test
+ttest2(error_without, error_with)
