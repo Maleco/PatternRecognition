0618121532a078de719e7019e301353f43d6f3af
Maleco <maikel.withagen@gmail.com>
2015-10-21 16:27:03 +0200

    ass 2

---
+++ lab6/Code/Ass2.m
@@ -0,0 +1,45 @@
+
+X = cluster_data;
+c_min = cluster(linkage(squareform(pdist(cluster_data)), 'single'), 'maxclust', 4);
+c_max = cluster(linkage(squareform(pdist(cluster_data)), 'complete'), 'maxclust', 4);
+c_avg = cluster(linkage(squareform(pdist(cluster_data)), 'average'), 'maxclust', 4);
+c_mean = cluster(linkage(squareform(pdist(cluster_data)), 'centroid'), 'maxclust', 4);
+
+hold on;
+figure();
+subplot(2,2,1)
+scatter(X(:,1),X(:,2),[],c_min, 'filled');
+hold on;
+for group = 1:4
+    plot(mean(X(c_min == group,1)),mean(X(c_min == group,2)), 'o', 'MarkerSize', 15, 'MarkerFacecolor', 'b');
+end
+xlabel('x');ylabel('y');title('Min');
+subplot(2,2,2   )
+scatter(X(:,1),X(:,2),[],c_max, 'filled')
+hold on;
+for group = 1:4
+    plot(mean(X(c_min == group,1)),mean(X(c_min == group,2)), 'o', 'MarkerSize', 15, 'MarkerFacecolor', 'b');
+end
+xlabel('x');ylabel('y');title('Max');
+subplot(2,2,3)
+scatter(X(:,1),X(:,2),[],c_avg, 'filled')
+hold on;
+for group = 1:4
+    plot(mean(X(c_min == group,1)),mean(X(c_min == group,2)), 'o', 'MarkerSize', 15, 'MarkerFacecolor', 'b');
+end
+xlabel('x');ylabel('y');title('Average');
+subplot(2,2,4)
+scatter(X(:,1),X(:,2),[],c_mean, 'filled')
+hold on;
+for group = 1:4
+    plot(mean(X(c_min == group,1)),mean(X(c_min == group,2)), 'o', 'MarkerSize', 15, 'MarkerFacecolor', 'b');
+end
+xlabel('x');ylabel('y');title('Mean');
+print(sprintf('../Report/Ass2_1'), '-depsc');
+ 
+figure();
+subplot(2,2,1); dendrogram(linkage(squareform(pdist(cluster_data)), 'single'), 270);    title('Min');
+subplot(2,2,2); dendrogram(linkage(squareform(pdist(cluster_data)), 'complete'), 270);title('Max');
+subplot(2,2,3); dendrogram(linkage(squareform(pdist(cluster_data)), 'average'), 270);title('Average');
+subplot(2,2,4); dendrogram(linkage(squareform(pdist(cluster_data)), 'centroid'), 270);title('Mean');
+print(sprintf('../Report/Ass2_2'), '-depsc');
