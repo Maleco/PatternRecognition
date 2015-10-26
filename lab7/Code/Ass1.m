244d00c0c77125e67aaf07bcffe6eb7d15912e7d
S1861948 <s1861948@fwn-bborg-edu-74-114.fmnsedu.rug.nl>
2015-10-26 17:46:50 +0100

    finished lab7

--- lab7/Code/Ass1.m
+++ lab7/Code/Ass1.m
@@ -1,14 +1,15 @@
+close all;
 load('mixedsignals.mat', 'smix');
 
 % Plot the initial values
 figure(1);
 plot(smix(1,:),smix(2,:), 'b.');
-xlabel('x');ylabel('y');title('Mixed signal x1 and x2 before sphering');
+xlabel('Signal 1');ylabel('Signal 2');title('Mixed signal x1 and x2 before sphering');
 print(sprintf('../Report/Ass1a'), '-depsc');
 
 figure(2);
 plot(smix(3,:),smix(4,:), 'b.');
-xlabel('x');ylabel('y');title('Mixed signal x3 and x4 before sphering');
+xlabel('Signal 3');ylabel('Signal 4');title('Mixed signal x3 and x4 before sphering');
 print(sprintf('../Report/Ass1b'), '-depsc');
 
 % Whitening
@@ -19,10 +20,10 @@ spheredS = eigVectors * (eigValues^-.5) * eigVectors.' * smix;
 % Plot the sphered values
 figure(3);
 plot(spheredS(1,:),spheredS(2,:), 'b.');
-xlabel('x');ylabel('y');title('Mixed signal x1 and x2 after sphering');
+xlabel('Signal 1');ylabel('Signal 2');title('Mixed signal x1 and x2 after sphering');
 print(sprintf('../Report/Ass1c'), '-depsc');
 
 figure(4);
 plot(spheredS(3,:),spheredS(4,:), 'b.');
-xlabel('x');ylabel('y');title('Mixed signal x3 and x4 after sphering');
+xlabel('Signal 3');ylabel('Signal 4');title('Mixed signal x3 and x4 after sphering');
 print(sprintf('../Report/Ass1d'), '-depsc');
